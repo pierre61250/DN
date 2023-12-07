f79cb769b4cd8e1a78457ef30821a8e01fdf987a
Pierre COLLET <pierre61250@yahoo.fr>
2023-12-07 22:59:29 +0100

    update

--- app/src/androidTest/java/com/example/dn/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/dn/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package com.example.dn
 
+import android.location.Location
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -20,5 +21,47 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.dn", appContext.packageName)
+    }
+
+    @Test
+    fun checkFileSavedLocationNotExist() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        // Check if file exist for save location exist
+        assertEquals(null, MainActivity.readLocationFile(appContext, "loc.txt"))
+    }
+
+    @Test
+    fun createFile() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val loc = Location("Test")
+        loc.latitude = 4.0
+        loc.longitude = 4.4
+
+        MainActivity.writeLocationFile(appContext, "loc.txt", loc)
+
+        assertNotEquals(null, MainActivity.readLocationFile(appContext, "loc.txt"))
+    }
+
+    @Test
+    fun checkValuesSavedInFile() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val loc = Location("Test")
+        loc.latitude = 4.0
+        loc.longitude = 4.4
+
+        MainActivity.writeLocationFile(appContext, "loc.txt", loc)
+
+        val locSaved = MainActivity.readLocationFile(appContext, "loc.txt")
+
+        if (locSaved != null) {
+            assertEquals(4.0, locSaved.latitude, 0.0001)
+            assertEquals(4.4, locSaved.longitude, 0.0001)
+        }
     }
 }
